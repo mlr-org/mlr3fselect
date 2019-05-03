6d0feba34c29125e7995b9e18c6ff48977021f3a
Michel Lang <michellang@gmail.com>
2019-05-03 12:51:00 +0200

    sugar to retrieve filters from dictionary

--- R/helpers.R
+++ R/helpers.R
@@ -1,10 +1,11 @@
 assert_filter = function (filter, task = NULL) {
-  assert_class(filter, "Filter")
+  filter = cast_from_dict(filter, "Filter", mlr_filters, FALSE)[[1L]]
+
   if (!is.null(task)) {
     if (task$task_type %nin% filter$task_type) {
       stopf("Filter '%s' is not compatible with type of task '%s' (type: %s)",
             filter$id, task$id, task$task_type)
     }
   }
-  invisible(filter)
+  return(filter)
 }
