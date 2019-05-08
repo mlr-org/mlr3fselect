a681adefaa9beca5a4e9ae9877eb8b47c40d1b73
Michel Lang <michellang@gmail.com>
2019-05-08 11:05:43 +0200

    renamed boston_housing task

--- tests/testthat/test_Filter.R
+++ tests/testthat/test_Filter.R
@@ -3,7 +3,7 @@ context("Filter")
 test_that("Errors for unsupported features", {
   # supported: numeric
   # supplied: factor, integer, numeric
-  task = mlr3::mlr_tasks$get("bh")
+  task = mlr3::mlr_tasks$get("boston_housing")
   filter = FilterLinearCorrelation$new()
   expect_error(filter$calculate(task))
 })
