f03a284f1bb3db39830ef6e5a9c8313e05aba13a
pat-s <patrick.schratz@gmail.com>
2019-06-20 17:51:16 +0200

    add carscore filter

---
+++ R/FilterCarScore.R
@@ -0,0 +1,43 @@
+#' @title Conditional Mutual Information Based Feature Selection Filter
+#'
+#' @aliases mlr_filters_carscore
+#' @format [R6::R6Class] inheriting from [FilterResult].
+#' @include FilterResult.R
+#'
+#' @description
+#' Filter `carscore` determines the Correlation-Adjusted (marginal) coRelation
+#' scores (short CAR scores). The CAR scores for a set of features are defined as the
+#' correlations between the target and the decorrelated features.
+#'
+#' @family Filter
+#' @export
+FilterCarScore = R6Class("FilterCarScore", inherit = FilterResult,
+  public = list(
+    initialize = function(id = "carscore") {
+      super$initialize(
+        id = id,
+        packages = "care",
+        feature_types = c("numeric"),
+        task_type = "regr"
+      )
+    }
+  ),
+
+  private = list(
+    .calculate = function(task) {
+
+      # FIXME task splitting should really be easier
+      data = as.data.table(task)
+      target = task$target_names
+      target = data[, ..target]
+
+      features = task$feature_names
+      features = as.data.frame(data[, ..features])
+
+      y = care::carscore(Xtrain = features, Ytrain = target, verbose = FALSE)^2
+      setNames(as.double(y), names(y))
+    }
+  )
+)
+
+register_filter("carscore", FilterCarScore)
