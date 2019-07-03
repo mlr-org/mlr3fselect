379331db4542728f05c5e61b942fc650ebc2542e
pat-s <patrick.schratz@gmail.com>
2019-07-03 21:37:49 +0200

    install ranger for pkgdown

--- tic.R
+++ tic.R
@@ -1,5 +1,9 @@
 do_package_checks(error_on = "error")
 
+# install ranger for README
+get_stage("install") %>%
+  add_step(step_install_cran("ranger"))
+
 if (ci_on_travis()) {
   do_pkgdown(orphan = TRUE, document = FALSE)
 }
