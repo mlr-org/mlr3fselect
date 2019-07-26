7c0ec0fab7f03315adb86299746881fb19ee6972
be-marc <marcbecker@posteo.de>
2019-07-26 16:19:29 +0200

    Remove filter content

--- tests/testthat/test_Filter.R
+++
@@ -1,9 +0,0 @@
-context("Filter")
-
-test_that("Errors for unsupported features", {
-  # supported: numeric
-  # supplied: factor, integer, numeric
-  task = mlr3::mlr_tasks$get("boston_housing")
-  filter = FilterLinearCorrelation$new()
-  expect_error(filter$calculate(task))
-})
--- tests/testthat/test_FilterAUC.R
+++
@@ -1,6 +0,0 @@
-context("FilterAUC")
-
-test_that("FilterAUC", {
-  f = FilterAUC$new()
-  expect_filter(f, task = mlr_tasks$get("sonar"))
-})
