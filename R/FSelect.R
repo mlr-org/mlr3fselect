fb55b5b22ea3a2db5ced80a372c770a6716a9ea3
be-marc <marcbecker@posteo.de>
2019-12-03 18:43:35 +0100

    Add methods to FSelect and update documentation

--- R/FSelect.R
+++ R/FSelect.R
@@ -4,26 +4,62 @@
 #'
 #' @description
 #' Abstract `FSelect` class that implements the base functionality each `FSelect*` class must provide.
+#' A [FSelect] object describes the feature selection strategy,
+#' i.e. how to optimize the black-box function
+#' and its feasible set defined by the [FSelectInstance] object.
+#'
+#' A list of measures can be passed to the instance, and they will always be all evaluated.
+#' However, single-criteria algortihms optimize only the first measure
+#'
+#' A [FSelect] object must write its result to the `assign_result` method of the [FSelectInstance] at the end in
+#' order to store the best selected feature subset and its estimated performance vector.
 #'
 #' @export
 FSelect = R6Class("FSelect",
   public = list(
     #' @field param_set [paradox::ParamSet]
+    #' @field packages `character()`
     param_set = NULL,
+    packages = NULL,
 
     #' @description
     #' Create new `FSelect` object.
     #' @param param_set [paradox::ParamSet]
+    #' @param packages `character()`
     #' Set of control parameter for the feature selection.
-    #' @return A `FSelect` object.
-    initialize = function(param_set) {
+    #' @return `FSelect` object.
+    initialize = function(param_set, packages = character()) {
       self$param_set = assert_param_set(param_set)
+      self$packages = assert_set(packages)
+    },
+
+    #' @description
+    #' Format method.
+    #' @return `character()
+    format = function() {
+      sprintf("<%s>", class(self)[1L])
+    },
+
+    #' @description
+    #' Print method.
+    #' @return `character()`
+    print = function() {
+      catf(format(self))
+      catf(str_indent("* Parameters:", as_short_string(self$param_set$values)))
+      catf(str_indent("* Packages:", self$packages))
     },
 
     #' @description
     #' Performce the feature selection on a [FSelectInstance] until termination.
     #' @param instance [FSelectInstance]
     select = function(instance) {
+      assert_r6(instance, "FSelectInstance")
+      require_namespaces(self$packages)
+
+      lg$info("Starting the feature selection with '%s' and '%s'" ,
+              self$format(), instance$terminator$format())
+      lg$info("Terminator settings: %s", as_short_string(instance$terminator$param_set$values))
+
       tryCatch({
         while (TRUE) {
           private$select_internal(instance)
--- R/assertions.R
+++ R/assertions.R
@@ -1,3 +1,7 @@
 assert_terminator = function(terminator) {
   assert_r6(terminator, "Terminator")
 }
+
+assert_set = function(x, empty = TRUE, .var.name = vname(x)) {
+  assert_character(x, min.len = as.integer(!empty), any.missing = FALSE, min.chars = 1L, unique = TRUE, .var.name = .var.name)
+}
