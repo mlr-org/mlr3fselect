003f06efcd2ee2be78a840ebcf632d95ad77dc2e
be-marc <marcbecker@posteo.de>
2019-12-04 00:54:18 +0100

    Add FSelectRandom max_features unit test

--- tests/testthat/test_FSelectRandom.R
+++ tests/testthat/test_FSelectRandom.R
@@ -3,4 +3,8 @@ context("FSelectRandom")
 test_that("FSelectRandom", {
   test_fselect("random", batch_size = 10, term_evals = 2, real_evals = 10)
   test_fselect("random", batch_size = 4, term_evals = 4, real_evals = 4)
+
+  z = test_fselect("random", max_features = 1, term_evals = 10)
+  a = z$inst$archive()
+  expect_features(a[, feat], n = 1)
 })
