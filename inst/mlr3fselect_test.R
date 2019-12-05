9cc8b80f08cc6fb0ec7f520ff0ee74c3af3cf767
be-marc <marcbecker@posteo.de>
2019-12-05 16:05:56 +0100

    Add FSelectRFE

--- inst/mlr3fselect_test.R
+++ inst/mlr3fselect_test.R
@@ -1,91 +1,17 @@
 library(mlr3)
 library(mlr3fselect)
+library(mlr3learners)
 
-# Random + Runtime
+# ref + evals
 task = tsk("pima")
-measure = msr("classif.acc")
+measure = msrs(c("classif.acc"))
 learner = lrn("classif.rpart")
 resampling = rsmp("cv")
-terminator = term("model_time", secs = 10)
+terminator = term("evals", n_evals = 8)
 
 instance = FSelectInstance$new(task, learner, resampling, measure, terminator)
-fs = fs("random")
+fs = fs("rfe", recursive = TRUE)
 fs$select(instance)
 
-instance$best()
-instance$best_by_batch(2)
-
-# Exhaustive + Runtime
-task = tsk("pima")
-measure = msr("classif.acc")
-learner = lrn("classif.rpart")
-resampling = rsmp("cv")
-terminator = term("model_time", secs = 5)
-
-instance = FSelectInstance$new(task, learner, resampling, measure, terminator)
-fs = fs("exhaustive")
-fs$select(instance)
-
-instance$best()
-instance$best_by_batch(2)
-
-# Exhaustive + Runtime
-task = tsk("pima")
-task$select(c("age", "insulin", "pregnant"))
-measure = msr("classif.acc")
-learner = lrn("classif.rpart")
-resampling = rsmp("cv")
-terminator = term("model_time", secs = 30)
-
-instance = FSelectInstance$new(task, learner, resampling, measure, terminator)
-fs = fs("exhaustive")
-fs$select(instance)
-
-instance$best()
-instance$best_by_batch(2)
 
 
-# Sequential Forward + Runtime
-task = tsk("pima")
-measure = msr("classif.acc")
-learner = lrn("classif.rpart")
-resampling = rsmp("cv")
-terminator = mlr3tuning::term("model_time", secs = 30)
-
-instance = FSelectInstance$new(task, learner, resampling, measure, terminator)
-fs = fs("sequential")
-fs$select(instance)
-
-instance$best()
-instance$best_by_batch(1)
-
-
-# Sequential Backward + Runtime
-task = tsk("pima")
-measure = msr("classif.acc")
-learner = lrn("classif.rpart")
-resampling = rsmp("cv")
-terminator = term("model_time", secs = 30)
-
-instance = FSelectInstance$new(task, learner, resampling, measure, terminator)
-fs = fs("sequential")
-fs$param_set$values = mlr3misc::insert_named(fs$param_set$values, list(strategy = "fsb"))
-fs$select(instance)
-
-instance$best()
-instance$best_by_batch(1)
-
-
-# Evolutionary + Runtime
-task = tsk("pima")
-measure = msr("classif.acc")
-learner = lrn("classif.rpart")
-resampling = rsmp("cv")
-terminator = term("model_time", secs = 10)
-
-instance = FSelectInstance$new(task, learner, resampling, measure, terminator)
-fs = fs("evolutionary")
-fs$select(instance)
-
-instance$best()
-instance$best_by_batch(2)
