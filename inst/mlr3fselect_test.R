d46051857cc94d0ced1bf2154b3f8e505417d36b
be-marc <marcbecker@posteo.de>
2019-12-05 16:07:35 +0100

    Add non-recursive example

--- inst/mlr3fselect_test.R
+++ inst/mlr3fselect_test.R
@@ -13,5 +13,16 @@ instance = FSelectInstance$new(task, learner, resampling, measure, terminator)
 fs = fs("rfe", recursive = TRUE)
 fs$select(instance)
 
+# ref + evals
+task = tsk("pima")
+measure = msrs(c("classif.acc"))
+learner = lrn("classif.rpart")
+resampling = rsmp("cv")
+terminator = term("evals", n_evals = 8)
+
+instance = FSelectInstance$new(task, learner, resampling, measure, terminator)
+fs = fs("rfe")
+fs$select(instance)
+
 
 
