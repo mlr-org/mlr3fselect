a026e666cd2190b202fcac80af76bb810e0d7724
be-marc <marcbecker@posteo.de>
2020-04-01 13:05:02 +0200

    Add term to reexport

--- R/reexports.R
+++ R/reexports.R
@@ -6,3 +6,20 @@
 #' @rdname mlr_terminators
 #' @export
 NULL
+
+#' @title Re-export of `term`
+#'
+#' See [bbotk::term].
+#'
+#' @name term
+#' @rdname term
+#' @export
+NULL
+
+#' @title Re-export of `terms`
+#'
+#' See [bbotk::terms].
+#'
+#' @name terms
+#' @rdname terms
+NULL
