20e904898b5f9ee0cc12b0c1605f1d35de25dbec
Marc Becker <marcbecker@posteo.de>
2020-10-27 16:43:45 +0100

    Reduce example run time

--- man-roxygen/example.R
+++ man-roxygen/example.R
@@ -1,7 +1,7 @@
 #' @examples
 #' library(mlr3)
 #'
-#' terminator = trm("evals", n_evals = 10)
+#' terminator = trm("evals", n_evals = 3)
 #'
 #' instance = FSelectInstanceSingleCrit$new(
 #'   task = tsk("iris"),
