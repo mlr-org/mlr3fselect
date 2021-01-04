2e568a4c0cecf7dc99e698423b50569a808752ac
Marc Becker <marcbecker@posteo.de>
2021-01-04 19:53:05 +0100

    Remove x_domain column

--- R/FSelectInstanceSingleCrit.R
+++ R/FSelectInstanceSingleCrit.R
@@ -77,7 +77,8 @@ FSelectInstanceSingleCrit = R6Class("FSelectInstanceSingleCrit",
       super$initialize(obj, obj$domain, terminator)
 
       self$archive = ArchiveFSelect$new(search_space = self$objective$domain,
-        codomain = self$objective$codomain, check_values = check_values)
+        codomain = self$objective$codomain, check_values = check_values,
+        store_x_domain = FALSE)
       self$objective$archive = self$archive
 
       private$.objective_function = objective_function
