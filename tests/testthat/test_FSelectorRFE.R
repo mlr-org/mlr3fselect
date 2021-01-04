7a796653f764a130019dc5137ca992905dbfb9c6
Marc Becker <marcbecker@posteo.de>
2021-01-04 19:25:59 +0100

    Add rfe importance to archive

--- tests/testthat/test_FSelectorRFE.R
+++ tests/testthat/test_FSelectorRFE.R
@@ -6,12 +6,14 @@ test_that("FSelectorRFE", {
   expect_feature_number(a[batch_nr == 1, 1:4], n = 4)
   expect_feature_number(a[batch_nr == 2, 1:4], n = 2)
   expect_feature_number(a[batch_nr == 3, 1:4], n = 1)
+  expect_true("importance" %in% names(a))
 
   z = test_fselector("rfe", recursive = TRUE, term_evals = 3L, store_models = TRUE)
   a = z$inst$archive$data
   expect_feature_number(a[batch_nr == 1, 1:4], n = 4)
   expect_feature_number(a[batch_nr == 2, 1:4], n = 2)
   expect_feature_number(a[batch_nr == 3, 1:4], n = 1)
+  expect_true("importance" %in% names(a))
 
   z = test_fselector("rfe", feature_fraction = 0.9, term_evals = 3L,
     store_models = TRUE)
