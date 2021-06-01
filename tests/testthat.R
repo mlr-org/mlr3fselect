b38c2010f1a584ec6431b2a3546102cc389b6d91
Marc Becker <marcbecker@posteo.de>
2021-06-01 17:26:36 +0200

    add fselect function family

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,6 +1,7 @@
-library(testthat)
-library(mlr3)
-library(mlr3fselect)
-library(checkmate)
-
-test_check("mlr3fselect")
+if (requireNamespace("testthat", quietly = TRUE)) {
+  library(testthat)
+  library(checkmate)
+  library(mlr3fselect)
+  
+  test_check("mlr3tuning")
+}
--- tests/testthat/test_AutoFSelector.R
+++ tests/testthat/test_AutoFSelector.R
@@ -148,23 +148,3 @@ test_that("AutoFSelector works with GraphLearner", {
   # Check for clone
   expect_equal(task$feature_names, c("x1", "x2", "x3" ,"x4"))
 })
-
-test_that("auto_fselector function works", {
-  afs = auto_fselector(method = "random_search", learner =  lrn("classif.rpart"), resampling = rsmp ("holdout"), 
-    measure = msr("classif.ce"), term_evals = 50, batch_size = 10)
-
-  expect_class(afs, "AutoFSelector")
-  expect_class(afs$instance_args$terminator, "TerminatorEvals")
-
-  afs = auto_fselector(method = "random_search", learner =  lrn("classif.rpart"), resampling = rsmp ("holdout"), 
-    measure = msr("classif.ce"), term_time = 50, batch_size = 10)
-
-  expect_class(afs, "AutoFSelector")
-  expect_class(afs$instance_args$terminator, "TerminatorRunTime")
-
-  afs = auto_fselector(method = "random_search", learner =  lrn("classif.rpart"), resampling = rsmp ("holdout"), 
-    measure = msr("classif.ce"), term_evals = 10, term_time = 50, batch_size = 10)
-
-  expect_class(afs, "AutoFSelector")
-  expect_class(afs$instance_args$terminator, "TerminatorCombo")
-})
