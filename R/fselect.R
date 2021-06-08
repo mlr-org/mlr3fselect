12c5788a1ed243d05b1ea5fa82bc86c728e4a8d5
Marc Becker <marcbecker@posteo.de>
2021-06-08 12:55:47 +0200

    restructure files

---
+++ R/fselect.R
@@ -0,0 +1,61 @@
+#' @title Function for Feature Selection
+#'
+#' @description
+#' Function to optimize the feature set of a [mlr3::Learner].
+#'
+#' @param method (`character(1)`)\cr
+#'  Key to retrieve fselector from [mlr_fselectors] dictionary.
+#' @param term_evals (`integer(1)`)\cr
+#'  Number of allowed evaluations.
+#' @param term_time (`integer(1)`)\cr
+#'  Maximum allowed time in seconds.
+#' @param ... (named `list()`)\cr
+#'  Named arguments to be set as parameters of the fselector.
+#'
+#' @return `FSelectInstanceSingleCrit`
+#'
+#' @template param_task
+#' @template param_learner
+#' @template param_resampling
+#' @template param_measure
+#'
+#' @export
+#' @examples
+#' task = tsk("pima")
+#'
+#' instance = fselect(
+#'   method = "random_search",
+#'   task = task,
+#'   learner = lrn("classif.rpart"),
+#'   resampling = rsmp ("holdout"),
+#'   measure = msr("classif.ce"),
+#'   term_evals = 50,
+#'   batch_size = 10)
+#'
+#' # subset task to optimized feature set
+#' task$select(instance$result_feature_set)
+fselect = function(method, task, learner, resampling, measure, term_evals = NULL, term_time = NULL, ...) {
+  assert_choice(method, mlr_fselectors$keys())
+  fselector = fs(method, ...)
+  terminator = terminator_selection(term_evals, term_time)
+
+  instance = FSelectInstanceSingleCrit$new(task, learner, resampling, measure, terminator)
+
+  fselector$optimize(instance)
+  instance
+}
+
+terminator_selection = function(term_evals, term_time) {
+  assert_int(term_evals, null.ok = TRUE)
+  assert_int(term_time, null.ok = TRUE)
+
+  if (is.null(term_evals) && is.null(term_time)) {
+    trm("none")
+  } else if (!is.null(term_evals) && !is.null(term_time)) {
+    trm("combo", list(trm("evals", n_evals = term_evals), trm("run_time", secs = term_time)))
+  } else if (!is.null(term_evals)) {
+    trm("evals", n_evals = term_evals)
+  } else if (!is.null(term_time)) {
+    trm("run_time", secs = term_time)
+  }
+}
---
+++ R/fselect_nested.R
@@ -0,0 +1,50 @@
+#' @title Function for Nested Resampling
+#'
+#' @description
+#' Function to conduct nested resampling.
+#'
+#' @param method (`character(1)`)\cr
+#'  Key to retrieve fselector from [mlr_fselectors] dictionary.
+#' @param inner_resampling ([mlr3::Resampling])\cr
+#'  Resampling used for the inner loop.
+#' @param outer_resampling [mlr3::Resampling])\cr
+#'  Resampling used for the outer loop.
+#' @param term_evals (`integer(1)`)\cr
+#'  Number of allowed evaluations.
+#' @param term_time (`integer(1)`)\cr
+#'  Maximum allowed time in seconds.
+#' @param ... (named `list()`)\cr
+#'  Named arguments to be set as parameters of the fselector.
+#'
+#' @return [mlr3::ResampleResult]
+#'
+#' @template param_task
+#' @template param_learner
+#' @template param_measure
+#'
+#' @export
+#' @examples
+#' rr = fselect_nested(
+#'   method = "random_search",
+#'   task = tsk("pima"),
+#'   learner = lrn("classif.rpart"),
+#'   inner_resampling = rsmp ("holdout"),
+#'   outer_resampling = rsmp("cv", folds = 2),
+#'   measure = msr("classif.ce"),
+#'   term_evals = 2,
+#'   batch_size = 2)
+#'
+#' # performance scores estimated on the outer resampling
+#' rr$score()
+#'
+#' # unbiased performance of the final model trained on the full data set
+#' rr$aggregate()
+fselect_nested = function(method, task, learner, inner_resampling, outer_resampling, measure, term_evals = NULL,
+  term_time = NULL, ...) {
+  assert_task(task)
+  assert_resampling(inner_resampling)
+  assert_resampling(outer_resampling)
+
+  afs = auto_fselector(method, learner, inner_resampling, measure, term_evals, term_time, ...)
+  resample(task, afs, outer_resampling, store_models = TRUE)
+}
