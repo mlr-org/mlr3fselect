d19a231eb086fd017addc2de46fc32088f4075ca
Michel Lang <michellang@gmail.com>
2022-01-20 21:28:38 +0100

    chore: unify testthat.R

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,7 +1,6 @@
 if (requireNamespace("testthat", quietly = TRUE)) {
-  library(testthat)
-  library(checkmate)
-  library(mlr3fselect)
-  
+  library("testthat")
+  library("checkmate")
+  library("mlr3fselect")
   test_check("mlr3fselect")
 }
