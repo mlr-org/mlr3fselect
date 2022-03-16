a70dbf9606ad334cc8ff1b90350c38d5a4787d16
Marc Becker <33069354+be-marc@users.noreply.github.com>
2022-03-16 18:20:57 +0100

    feat: Allow to pass FSelector objects as method in fselect() and auto_fselector() (#51)

--- tests/testthat/test_fselect.R
+++ tests/testthat/test_fselect.R
@@ -1,6 +1,6 @@
 test_that("fselect function works with single measure", {
-  instance = fselect(method = "random_search", task = tsk("pima"), learner = lrn("classif.rpart"), resampling = rsmp ("holdout"),
-    measures = msr("classif.ce"), term_evals = 2, batch_size = 1)
+  instance = fselect(method = fs("random_search", batch_size = 1), task = tsk("pima"), learner = lrn("classif.rpart"), resampling = rsmp ("holdout"),
+    measures = msr("classif.ce"), term_evals = 2)
 
   expect_class(instance, "FSelectInstanceSingleCrit")
   expect_data_table(instance$archive$data, nrows = 2)
@@ -8,10 +8,19 @@ test_that("fselect function works with single measure", {
 })
 
 test_that("fselect function works with multiple measures", {
-  instance = fselect(method = "random_search", task = tsk("pima"), learner = lrn("classif.rpart"), resampling = rsmp ("holdout"),
-    measures = msrs(c("classif.ce", "classif.acc")), term_evals = 2, batch_size = 1)
+  instance = fselect(method = fs("random_search", batch_size = 1), task = tsk("pima"), learner = lrn("classif.rpart"), resampling = rsmp ("holdout"),
+    measures = msrs(c("classif.ce", "classif.acc")), term_evals = 2)
 
   expect_class(instance, "FSelectInstanceMultiCrit")
+  expect_data_table(instance$archive$data, nrows = 2)
+  expect_class(instance$terminator, "TerminatorEvals")
+})
+
+test_that("fselect function accepts string input for method", {
+  instance = fselect(method = "random_search", task = tsk("pima"), learner = lrn("classif.rpart"), resampling = rsmp ("holdout"),
+    measures = msr("classif.ce"), term_evals = 2, batch_size = 1)
+
+  expect_class(instance, "FSelectInstanceSingleCrit")
   expect_data_table(instance$archive$data, nrows = 2)
   expect_class(instance$terminator, "TerminatorEvals")
 })
