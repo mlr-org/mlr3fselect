a563ec81425ac4b53912d87f492da5919ad3bff6
john <bblodfon@gmail.com>
2024-05-16 19:11:31 +0200

    update test

--- tests/testthat/test_ensemble_fselect.R
+++ tests/testthat/test_ensemble_fselect.R
@@ -1,15 +1,19 @@
-test_that("esemble feature selection works", {
+test_that("ensemble feature selection works", {
   res = ensemble_fselect(
-    fselector = fs("random_search"),
+    fselector = fs("rfe", n_features = 2, feature_fraction = 0.8),
     task = tsk("sonar"),
     learners = lrns(c("classif.rpart", "classif.featureless")),
-    outer_resampling = rsmp("subsampling", repeats = 2),
+    init_resampling = rsmp("subsampling", repeats = 2),
     inner_resampling = rsmp("cv", folds = 3),
     measure = msr("classif.ce"),
-    terminator = trm("evals", n_evals = 10)
+    terminator = trm("none")
   )
 
   expect_data_table(res, nrows = 4)
+  expect_list(res$features, any.missing = FALSE, len = 4)
+  expect_vector(res$n_features, size = 4)
+  expect_vector(res$classif.ce, size = 4)
+  expect_list(res$importance, any.missing = FALSE, len = 4)
 })
 
 
