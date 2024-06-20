78858e672cb4b875735b080127ceec45af6c572c
john <bblodfon@gmail.com>
2024-06-20 19:51:52 +0200

    more realistic pareto front in the test

--- tests/testthat/test_ensemble_fselect.R
+++ tests/testthat/test_ensemble_fselect.R
@@ -153,7 +153,7 @@ test_that("EnsembleFSResult initialization", {
       c("V2"),
       c("V4", "V12"),
       c("V6", "V15", "V19", "V7")),
-    classif.ce = c(0.13, 0.24, 0.16, 0.11, 0.25, 0.18, 0.09, 0.1, 0.16)
+    classif.ce = c(0.13, 0.24, 0.16, 0.11, 0.25, 0.18, 0.15, 0.1, 0.16)
   )
 
   # works without benchmark result object
