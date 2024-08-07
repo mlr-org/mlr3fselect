fa05f09cdc8126d6fdff756bedc140ac5b6766f7
john <bblodfon@gmail.com>
2024-08-07 13:48:12 +0200

    update tests

--- tests/testthat/test_voting_methods.R
+++ tests/testthat/test_voting_methods.R
@@ -22,16 +22,20 @@ w2_equal = rep(1, length(vot2))
 test_that("approval voting", {
   # large data
   av = approval_voting(vot, cand, w)
-  expect_data_table(av, nrows = length(cand), ncols = 3)
-  expect_setequal(colnames(av), c("feature", "score", "norm_score"))
+  expect_data_table(av, nrows = length(cand), ncols = 4)
+  expect_setequal(colnames(av), c("feature", "score", "norm_score", "borda_score"))
   expect_setequal(av$feature, cand) # all features are there
   expect_true(all(av$score >= 0)) # positive scores
   expect_true(all(av$norm_score >= 0 & av$norm_score <= 1)) # behave like probs
+  expect_equal(av$borda_score[1], 1)
+  expect_equal(av$borda_score[length(cand)], 0)
 
   av_equalw = approval_voting(vot, cand, w_equal)
-  expect_data_table(av_equalw, nrows = length(cand), ncols = 3)
+  expect_data_table(av_equalw, nrows = length(cand), ncols = 4)
   expect_true(all(av_equalw$score >= 0)) # positive scores
   expect_true(all(av_equalw$norm_score >= 0 & av_equalw$norm_score <= 1)) # behave like probs
+  expect_equal(av_equalw$borda_score[1], 1)
+  expect_equal(av_equalw$borda_score[length(cand)], 0)
   # using unequal weights, feature rankings should be different
   expect_false(identical(av$feature, av_equalw$feature))
 
@@ -52,16 +56,20 @@ test_that("approval voting", {
 test_that("satisfaction approval voting", {
   # large data
   sav = satisfaction_approval_voting(vot, cand, w)
-  expect_data_table(sav, nrows = length(cand), ncols = 3)
-  expect_setequal(colnames(sav), c("feature", "score", "norm_score"))
+  expect_data_table(sav, nrows = length(cand), ncols = 4)
+  expect_setequal(colnames(sav), c("feature", "score", "norm_score", "borda_score"))
   expect_setequal(sav$feature, cand) # all features are there
   expect_true(all(sav$score >= 0)) # positive scores
   expect_true(all(sav$norm_score >= 0 & sav$norm_score <= 1)) # behave like probs
+  expect_equal(sav$borda_score[1], 1)
+  expect_equal(sav$borda_score[length(cand)], 0)
 
   sav_equalw = satisfaction_approval_voting(vot, cand, w_equal)
-  expect_data_table(sav_equalw, nrows = length(cand), ncols = 3)
+  expect_data_table(sav_equalw, nrows = length(cand), ncols = 4)
   expect_true(all(sav_equalw$score >= 0)) # positive scores
   expect_true(all(sav_equalw$norm_score >= 0 & sav_equalw$norm_score <= 1)) # behave like probs
+  expect_equal(sav_equalw$borda_score[1], 1)
+  expect_equal(sav_equalw$borda_score[length(cand)], 0)
   # using unequal weights, feature rankings should be different
   expect_false(identical(sav$feature, sav_equalw$feature))
 
