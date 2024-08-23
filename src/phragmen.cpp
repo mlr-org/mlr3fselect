6b7fb036bba0427aebded5907b8f6741a1815923
john <bblodfon@gmail.com>
2024-08-23 14:48:23 +0200

    optimize Phragmen a bit => do not randomly select the candidate with min load

--- src/phragmen.cpp
+++ src/phragmen.cpp
@@ -20,17 +20,15 @@ List seq_Phragmen_rcpp(List voters, CharacterVector candidates, NumericVector we
   CharacterVector committee; // starts empty
   CharacterVector remaining_candidates = clone(candidates); // starts with all candidates
 
-  // Build a vector of indexes corresponding to the voters that approved each candidate
+  // Build a vector of voter indexes for each candidate: which voters approved a candidate
   std::vector<std::vector<int>> approving_voters(candidates.size());
 
   // For every candidate
   for (int i = 0; i < candidates.size(); ++i) {
     String candidate = candidates[i];
-    // Get each voter's candidates
     for (int j = 0; j < voters.size(); ++j) {
       // Get the candidates of voter j
       CharacterVector voter_candidates = voters[j];
-
       // Check if the candidate is in the voter's list
       bool candidate_found = std::find(voter_candidates.begin(), voter_candidates.end(), candidate) != voter_candidates.end();
       if (candidate_found) {
@@ -45,54 +43,45 @@ List seq_Phragmen_rcpp(List voters, CharacterVector candidates, NumericVector we
   // build committee sequentially, stop at `n_candidates`
   for (int i = 0; i < n_candidates; ++i) {
     int n_remaining_candidates = remaining_candidates.size();
-    int index;
+    int index = 0; // index of 'remaining_candidates' with min load
+    double min_load = std::numeric_limits<double>::max(); // +Inf
     String best_candidate;
-    std::vector<double> new_loads(n_remaining_candidates, std::numeric_limits<double>::max());
+    //Rcout << "\nRemaining candidates: " << remaining_candidates << "\n";
 
-    // Compute the loads for approving voters if we are adding a candidate
+    // Compute the new loads for approving voters if we are adding a candidate
+    // and maintain the minimum one
     for (int j = 0; j < n_remaining_candidates; ++j) {
-      double total_weight = 0.0;
-      double sum_current_loads = 0.0;
+      double total_weight = 0;
+      double sum_current_loads = 0;
       String candidate = remaining_candidates[j];
-      int candidate_index = get_index(candidates, candidate);
-      Rcout << candidate.get_cstring() << " " << candidate_index << std::endl;
 
+      // need original candidate index for accessing 'approving_voters'
+      int candidate_index = get_index(candidates, candidate);
       for (int v : approving_voters[candidate_index]) {
         sum_current_loads += current_loads[v];
         total_weight += weights[v];
       }
-      new_loads[i] = (1.0 + sum_current_loads) / total_weight;
-    }
-    Rcout << "new_loads: ";
-    for (const auto& load : new_loads) {
-      Rcout << load << " ";
-    }
-    Rcout << std::endl;
-
-
-    // Find the minimum load
-    double min_load = *std::min_element(new_loads.begin(), new_loads.end());
-
-    // Find candidate with minimum load
-    std::vector<int> min_indices;
-    for (int j = 0; j < new_loads.size(); ++j) {
-      if (new_loads[j] == min_load) {
-        min_indices.push_back(j);
+      double new_load = (1.0 + sum_current_loads) / total_weight;
+      //Rcout << "New load: " << new_load << ", min prev index: " << index << " Min: " << min_load << "\n";
+      if (new_load < min_load) {
+        min_load = new_load;
+        index = j;
       }
     }
 
-    // Get random index from those that have min load
-    index = min_indices[std::floor(R::runif(0, min_indices.size()))];
+    // Get candidate with min load
     best_candidate = remaining_candidates[index];
+    //Rcout << "Best candidate: " << best_candidate.get_cstring() << "\n";
 
     // Add best candidate to the committee
     committee.push_back(best_candidate);
     // Remove best candidate from the remaining candidates
     remaining_candidates.erase(index);
-    // Update current loads
+    // need original candidate index for accessing 'approving_voters'
     int best_candidate_index = get_index(candidates, best_candidate);
+    // Update current loads
     for (int v : approving_voters[best_candidate_index]) {
-      current_loads[v] = new_loads[index];
+      current_loads[v] = min_load;
     }
   }
 
